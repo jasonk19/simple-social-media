a3b0cd49137750bb1c6a4bb73ffce8542d044627
jasonk19 <13520080@std.stei.itb.ac.id>
2022-07-28 21:05:11 +0700

    change stylesheet to sass

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Routing } from "./routing";
-import "./App.css";
 
 function App() {
   return (
